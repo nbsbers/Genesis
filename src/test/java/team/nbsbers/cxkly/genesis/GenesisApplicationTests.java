3ea69e0f6af742c2cf7446733cd46582cb40dbbb
Echo <819905193@qq.com>
2024-06-19 02:09:20 +0800

    Initial commit

---
+++ src/test/java/team/nbsbers/cxkly/genesis/GenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package team.nbsbers.cxkly.genesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
