b0a783b351caf123206faf0a48a8c2d0aba18f39
Echo <819905193@qq.com>
2024-06-19 20:59:53 +0800

    some initialization

--- src/test/java/team/nbsbers/cxkly/genesis/GenesisApplicationTests.java
+++ src/test/java/team/nbsbers/cxkly/genesis/GenesisApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GenesisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
