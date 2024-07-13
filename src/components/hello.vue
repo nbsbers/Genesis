bf3ca49d592de8684c193d351743b47f40b4d6de
Echo <819905193@qq.com>
2024-07-14 02:11:38 +0800

    init

---
+++ src/components/hello.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  let name = 'zj'
+  let age = 20
+  let tel = '15888888888'
+</script>
+
+<template>
+  <div>
+    <h1>hello</h1>
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <h2>电话：{{tel}}</h2>
+  </div>
+</template>
+
+<style>
+
+</style>
